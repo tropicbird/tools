c1bdee713dc5fc2d07f11f3cfeff6e766788230c
tropicbird <34531271+tropicbird@users.noreply.github.com>
2021-04-01 17:06:07 +0900

    Update change.bat

--- change.bat
+++ change.bat
@@ -1,9 +1,10 @@
+@rem ファイル名の一部を削除するバッチファイル
 echo off
 for %%f in ( * ) do call :sub "%%f"
 exit /b
 
 :sub
 set fname=%1
-set fname=%fname:Mesh1km3dim2016_SetaiThresh025_=""%
+set fname=%fname:ここに削除する文字を記載=""%
 ren %1 %fname%
 goto :EOF
